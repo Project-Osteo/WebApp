d532b20b0415c0fa8b76310e4cc8affa7ff3c68e
João Duarte <2181984@my.ipleiria.pt>
2020-11-15 10:58:43 +0000

    api

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 //Listar todos os pacientes
-app.get('/api/get/pacientes', (req, res) => {
+app.get('/api/pacientes', (req, res) => {
     const sqlSelectAllPacientes = 'SELECT * FROM pacientes';
     db.query(sqlSelectAllPacientes, (err, result) => {
         res.send(result);
@@ -25,19 +25,38 @@ app.get('/api/get/pacientes', (req, res) => {
 });
 
 //Listar todas as consultas
-app.get('/api/get/consultas',  (req, res) => {
+app.get('/api/consultas',  (req, res) => {
     const sqlSelectAllConsultas = 'SELECT * FROM consultas';
     db.query(sqlSelectAllConsultas, (err, result) => {
         res.send(result);
     })
 });
 
+//Listar consultas do paciente
+app.get('/api/pacientes/:id/consultas', (req, res) => {
+    var id = req.params.paciente_id;
+    const sqlSelectConsultasFromPaciente = "SELECT * FROM consultas WHERE paciente_id = ?";
+    db.query(sqlSelectConsultasFromPaciente, [id], (err, result) => {
+        console.log(result);
+    });
+});
+
 //Listar todos os treinos
-app.get('/api/get/treinos',  (req, res) => {
+app.get('/api/treinos',  (req, res) => {
     const sqlSelectAllTreinos = 'SELECT * FROM consultas';
     db.query(sqlSelectAllTreinos, (err, result) => {
         res.send(result);
     })
+});
+
+//Listar treinos do paciente
+app.get('/api/pacientes/:id/treinos', (req, res) => {
+    var id = req.params.paciente_id;
+    const sqlSelectTreinosFromPaciente = "SELECT * FROM treinos WHERE paciente_id = ?";
+    db.query(sqlSelectTreinosFromPaciente, (err, result) => {
+        console.log(result);
+    });
+
 });
 
 //Inserir novo utilizador
@@ -48,6 +67,34 @@ app.post('/api/registar', (req, res) => {
 
     const sqlInsertUser = "INSERT INTO utilizadores (nome, mail, pwd) VALUES (?,?,?)";
     db.query(sqlInsertUser, [nome, email, password], (err, result) => {
+        console.log(result);
+    });
+});
+
+//Inserir dados paciente POR ACABAR
+app.post('/api/utilizador/inserirdados', (req, res) => {
+    const user_id;
+    const nome;
+    const mail;
+    const genero;
+    const nacionalidade;
+    const localidade;
+    const data_nascimento;
+    const altura;
+    const telemovel;
+
+    const sqlInsertDadosPaciente = "INSERT INTO PACIENTES " +
+        "(user_id, nome, mail, genero, nacionalidade, localidade, data_nascimento, altura, telemovel)"
+
+
+
+});
+
+//Dados paciente
+app.get('/api/utilizador/:id', (req, res) => {
+    var id = req.params.id
+    const sqlSelectDadosPaciente = "SELECT * FROM pacientes WHERE user_id = ?";
+    db.query(sqlSelectDadosPaciente, (err, result) => {
         console.log(result);
     });
 });
