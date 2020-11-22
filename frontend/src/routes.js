adbde042d5b3e9bc92e4ce54a709b77e6444a24f
jbrigido <jbrigido_14@hotmail.com>
2020-11-22 23:38:08 +0000

    rota homepage

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Homepage from './pages/Homepage';
 
 export default function Routes() {
     return (
       <BrowserRouter>
         <Switch>
-            <Route path="/" component={Login} />
+            <Route path="/" exact component={Login} />
+            <Route path="/homepage" component={Homepage} />
         </Switch>
       </BrowserRouter>  
     );
