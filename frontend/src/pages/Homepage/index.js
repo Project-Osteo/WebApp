3ff16af5d17d0ed60a5d1531541491e678ccc187
jbrigido <jbrigido_14@hotmail.com>
2020-11-23 00:11:34 +0000

    inicio homepage

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 export default function Homepage() {
    return(
-       <h1>Homepage</h1>
+      <div className="homepage-container">
+          <header>
+              <span>Bem-vindo Osteo</span>
+    
+          </header>
+
+        <ul>
+            <li>
+                <strong>
+                    Jona do Cota
+                </strong>
+            </li>
+            <li>
+                <strong>
+                    Jona do Cota
+                </strong>
+            </li>
+            <li>
+                <strong>
+                    Jona do Cota
+                </strong>
+            </li>
+            <li>
+                <strong>
+                    Jona do Cota
+                </strong>
+            </li>
+        </ul>
+      </div> 
    );
 }
