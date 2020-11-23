14861176feda3c05aff15b2ac4cf7d6d6eb16206
jbrigido <jbrigido_14@hotmail.com>
2020-11-23 17:48:55 +0000

    inicio da pagina de pacientes

--- frontend/src/pages/Pacientes/index.js
+++ frontend/src/pages/Pacientes/index.js
@@ -5,6 +5,16 @@ import './styles.css';
 
 export default function Pacientes() {
     return(
-        <h1>Pacientes</h1>
+        <div className="pacientes-container">
+
+            <header>
+                <p><b>Nome:</b> Jona do cota</p>
+                <p><b>Idade:</b> 28 anos</p>
+                <p><b>Localidade:</b> Charca de Famões</p>
+                <p><b>Peso:</b> 70kg</p>
+                <p><b>Altura:</b> 1,80m</p>
+            </header>
+
+        </div>
     );
 }
