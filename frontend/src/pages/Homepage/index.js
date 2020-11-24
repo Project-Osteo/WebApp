7221aff74049466ab14d88e4c9a6f1de8eef1df1
jbrigido <jbrigido_14@hotmail.com>
2020-11-24 11:11:32 +0000

    botoes homepage

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiUserPlus, FiSettings, FiPower } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -8,6 +9,19 @@ export default function Homepage() {
       <div className="homepage-container">
           <header>
               <span>Bem-vindo Osteo</span>
+
+                <Link to="/pacientes">
+                    <FiUserPlus size={18} />
+                </Link>
+
+                <Link>
+                    <FiSettings size={18} />
+                </Link>
+
+                <Link>
+                    <FiPower size={18} />
+                </Link>
+
           </header>
 
         <ul>
