880814627075217cd2c258f2fa9b9d2369b7b61e
jbrigido <jbrigido_14@hotmail.com>
2020-11-24 14:39:54 +0000

    estilos pagina pacientes

--- frontend/src/pages/Pacientes/index.js
+++ frontend/src/pages/Pacientes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiSettings, FiPower } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -8,12 +9,27 @@ export default function Pacientes() {
         <div className="pacientes-container">
 
             <header>
-                <p><b>Nome:</b> Jona do cota</p>
-                <p><b>Idade:</b> 28 anos</p>
-                <p><b>Localidade:</b> Charca de Famões</p>
-                <p><b>Peso:</b> 70kg</p>
-                <p><b>Altura:</b> 1,80m</p>
+
+                <span>Paciente: Jona do cota</span>
+
+                <div className="btn-group">
+                    <Link>
+                        <FiSettings size={55} color="#41414d"></FiSettings>
+                    </Link>
+
+                    <Link>
+                        <FiPower size={55} color="#41414d"></FiPower>
+                    </Link>
+                </div>
+                
             </header>
+
+            <div className="info-paciente">
+                    <p><b>Idade:</b> 28 anos</p>
+                    <p><b>Localidade:</b> Charca de Famões</p>
+                    <p><b>Peso:</b> 70kg</p>
+                    <p><b>Altura:</b> 1,80m</p>
+            </div>
             
         </div>
     );
