ede91afa0a60b785919cbbc8648a316801e435e5
jbrigido <jbrigido_14@hotmail.com>
2020-11-28 18:06:06 +0000

    formulario novo paciente

--- frontend/src/pages/NovoPaciente/index.js
+++ frontend/src/pages/NovoPaciente/index.js
@@ -2,24 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiHome, FiSettings, FiPower } from 'react-icons/fi';
 
+import './styles.css';
+
 export default function NovoPaciente () {
     return(
         <div className="novoPaciente-container">
-            <section>
-                <h1>Adicionar um novo paciente</h1>
-            </section>
+            <div className="content">
+                <section>
+                    <h1>Adicionar um novo paciente</h1>
+                </section>
 
-            <form>
-                <input placeholder="Nome" />
-                <input placeholder="E-mail" />
-                <input placeholder="Género" />
-                <input placeholder="Nacionalidade" />
-                <input placeholder="Localidade" />
-                <input placeholder="Data de nascimento" />
-                <input placeholder="Altura" />
-                <input placeholder="Contacto" />
-                <button>ADICIONAR</button>
-            </form>
+                <form>
+                    <input placeholder="Nome" />
+                    <input placeholder="E-mail" />
+                    <input placeholder="Género" />
+                    <input placeholder="Nacionalidade" />
+                    <input placeholder="Localidade" />
+                    <input placeholder="Data de nascimento" />
+                    <input placeholder="Altura" />
+                    <input placeholder="Contacto" />
+                    <Link type="submit" to="/homepage">ADICIONAR</Link>
+                </form>
+            </div>
         </div>
     );
 }
