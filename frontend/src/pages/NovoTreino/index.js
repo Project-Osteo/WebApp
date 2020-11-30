aad2a29f542de0efd15ed6dc023beb146436f9bc
jbrigido <jbrigido_14@hotmail.com>
2020-11-30 19:59:25 +0000

    inicio pagina de novo treino

--- frontend/src/pages/NovoTreino/index.js
+++ frontend/src/pages/NovoTreino/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiHome, FiSettings, FiFilePlus, FiPower } from 'react-icons/fi';
 
+import './styles.css';
+
 export default function NovoTreino (){
     return(
-        <h1>Novo Treino</h1>
+       <div className="novoTreino-container">
+
+            <section>
+
+                <form>
+                    <h1>Adicionar novo treino</h1>
+
+                    <input placeholder="Descrição" />
+                    <input placeholder="Tipo de Treino" />
+                    <input placeholder="Observações" />
+                    <Link type="subomit" to="/pacientes">ADICIONAR</Link>
+                </form>
+
+            </section>
+
+       </div>
     );
 }
