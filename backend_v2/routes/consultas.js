0207848501e35829de926f3534f96fb813149cd4
jotaduart <jotaduart@gmail.com>
2020-12-24 00:54:36 +0000

    post consultas api

--- backend_v2/routes/consultas.js
+++ backend_v2/routes/consultas.js
@@ -30,5 +30,35 @@ router.get('/:id', (req, res, next) => {
     });
 });
 
+//NEW CONSULTA
+router.post('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `INSERT INTO Consultas 
+            (data, descricao, paciente_id, terapeuta_id, peso, tratamento, obs, recomendacao) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
+            [
+                req.body.data, 
+                req.body.descricao, 
+                req.body.paciente_id,
+                req.body.terapeuta_id, 
+                req.body.peso, 
+                req.body.tratamento, 
+                req.body.obs,
+                req.body.recomendacao                
+            ],
+            (error, result, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                res.status(201).send({
+                    mensagem: 'Consulta criada com sucesso!',
+                    consulta_id: result.insertId
+                });
+            }
+        )
+    });
+});
+
 
 module.exports = router;
