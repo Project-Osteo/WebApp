561c30d01536b2d1ac5990b8c9a889757228094b
jbrigido <jbrigido_14@hotmail.com>
2021-01-24 22:15:42 +0000

    form nova consulta

--- frontend/src/pages/NovaConsulta/index.js
+++ frontend/src/pages/NovaConsulta/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
 import { FiHome, FiSettings, FiPower, FiArrowLeft } from 'react-icons/fi';
 import Navbar from 'react-bootstrap/Navbar';
-import { Form, Nav, Button } from 'react-bootstrap';
+import { Form, Nav, Button, Col, Row } from 'react-bootstrap';
 import axios from 'axios';
 
 import './styles.css';
@@ -22,7 +22,7 @@ export default function NovaConsulta (){
     const submitConsulta = async () => {
         let res = await axios.post(`http://localhost:3001/consultas/${id}`,
         { data_consulta: data, descricao_consulta: descricao, tratamento: tratamento, obs_consulta: obs, recomendacao: rec })
-            history.push(`/pacientes/${id}`);
+            /*history.push(`/pacientes/${id}`);*/
             console.log(res);
     }
 
@@ -106,34 +106,46 @@ export default function NovaConsulta (){
             </div>*/}
 
             <Form>
-            <Form.Group controlId="descricaoConsulta">
-                <Form.Label>Descrição da Consulta</Form.Label>
-                <Form.Control as="textarea" rows={4} type="text" value={descricao} 
-                    onChange={(e) => setDescricao(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="tratamento">
-                <Form.Label>Tratamento</Form.Label>
-                <Form.Control as="textarea" rows={4} type="text" value={tratamento} 
-                   onChange={(e) => setTratamento(e.target.value)} /> 
-            </Form.Group>
-            <Form.Group controlId="recomendacoes">
-                <Form.Label>Recomendações</Form.Label>
-                <Form.Control as="textarea" rows={4} type="text" value={rec}
-                    onChange={(e) => setRec(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="observacoes">
-                <Form.Label>Observações</Form.Label>
-                <Form.Control as="textarea" rows={4} type="text" value={obs}
-                    onChange={(e) => setObs(e.target.value)} />
-            </Form.Group>
             <Form.Group controlId="dataConsulta">
+                <Col sm="4">
                 <Form.Label>Data da Consulta(AAAA-MM-DD)</Form.Label>
                 <Form.Control type="text" value={data}
                     onChange={(e) => setData(e.target.value)} />
+                </Col>
             </Form.Group>
-            <Button variant="secondary" type="submit" onClick={submitConsulta}>
+            <Form.Group controlId="descricaoConsulta">
+                <Col sm="12">
+                <Form.Label>Descrição da Consulta</Form.Label>
+                <Form.Control as="textarea" rows={3} type="text" value={descricao} 
+                    onChange={(e) => setDescricao(e.target.value)} />
+                </Col>
+            </Form.Group>
+            <Form.Group controlId="tratamento">
+                <Col sm="12">
+                <Form.Label>Tratamento</Form.Label>    
+                <Form.Control as="textarea" rows={3} type="text" value={tratamento} 
+                    onChange={(e) => setTratamento(e.target.value)} /> 
+                </Col>
+            </Form.Group>
+            <Form.Group controlId="recomendacoes">
+                <Col sm="12">
+                <Form.Label>Recomendações</Form.Label>   
+                <Form.Control as="textarea" rows={3} type="text" value={rec}
+                    onChange={(e) => setRec(e.target.value)} />
+                </Col>
+            </Form.Group>
+            <Form.Group controlId="observacoes">
+                <Col sm="12">
+                <Form.Label>Observações</Form.Label>
+                <Form.Control as="textarea" rows={3} type="text" value={obs}
+                    onChange={(e) => setObs(e.target.value)} />
+                </Col>
+            </Form.Group>
+            <Col sm="2">
+            <Button variant="secondary" type="submit" onClick={submitConsulta} href={'/pacientes/' + id}>
                 Guardar
             </Button>
+            </Col>
             </Form>
         </div>
     );
