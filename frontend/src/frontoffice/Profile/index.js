102767188d78387e65aa87a84c0609de3826a045
jbrigido <jbrigido_14@hotmail.com>
2021-02-09 17:01:14 +0000

    frontoffice - estatisticas

--- frontend/src/frontoffice/Profile/index.js
+++ frontend/src/frontoffice/Profile/index.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams,useHistory } from 'react-router-dom';
-import { FiHome, FiSettings, FiPower, FiArrowLeft, FiEdit2, FiTrash2, FiSave } from 'react-icons/fi';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as ReactBootStrap from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
-import { Nav, Col, Form, Button, Modal, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+import { Nav, CardGroup } from 'react-bootstrap';
 import axios from "axios";
 
 import './styles.css';
 
 export default function Profile () {
     let { id } = useParams();
+
+    const [statsInfo, setStatsInfo] = useState({});
+
+    const [numConsultas, setNumConsultas] = useState(statsInfo.numConsultas);
+    const [numTreinos, setNumTreinos] = useState(statsInfo.numTreinos);
+    const [numFeedbacks, setNumFeedbacks] = useState(statsInfo.numFeedbacks);
+
+    useEffect(() => {
+        axios
+        .get(`http://localhost:3001/pacientes/${id}/stats`)
+        .then((response) => {
+            var result = response.data;
+            setStatsInfo(result);
+            setNumConsultas(result.n_consultas);
+            setNumTreinos(result.n_treinos);
+            setNumFeedbacks(result.n_feedbacks);        
+        });
+    }, []);
 
     return (
         <div className="profile-container">
@@ -26,6 +43,38 @@ export default function Profile () {
                     </Navbar.Collapse>
                 </Navbar>
             </header>
+
+            <div className="container">
+
+            <CardGroup>
+            <Card>
+                <Card.Body>
+                <Card.Title style={{ fontSize: '50px' }}>{numConsultas}</Card.Title>
+                <Card.Text style={{ fontSize: '25px' }}>
+                    Número de consultas em que esteve presente.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                <Card.Title style={{ fontSize: '50px' }}>{numFeedbacks}</Card.Title>
+                <Card.Text style={{ fontSize: '25px' }}>
+                    Número de feedbacks que você atribuiu às consultas.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                <Card.Title style={{ fontSize: '50px' }}>{numTreinos}</Card.Title>
+                <Card.Text style={{ fontSize: '25px' }}>
+                    Número de treinos que você realizou.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            </CardGroup>
+
+            </div>
+
         </div>
     );
 }
